99a138f41b389997f3b9320ac3d62061cd43024f
SpongeStark <yksponge@gmail.com>
2025-03-25 00:15:05 +0100

    dataset class for BIPEDv2

--- src/dataset.py
+++ src/dataset.py
@@ -14,10 +14,13 @@ from ImageProcessing.ImageAnalyzer import ImageAnalyzer
 from ImageProcessing.ConvolutionFilter import Filter
 from Parameters import WEIGHT_LIST, SIGMA_LIST
 
+ANALYSER = ImageAnalyzer(
+    Filter(distance_from_screen=50, sigma_list=SIGMA_LIST, weight_list=WEIGHT_LIST) # sDoG filter
+)
 
 
 class BIPEDv2(Dataset):
-    def __init__(self, ori_path, gt_path):
+    def __init__(self, ori_path, gt_path, analyer=ANALYSER):
         self.ori_path = Path(ori_path)
         self.gt_path = Path(gt_path)
         self.indexes = list(set([x.stem for x in self.ori_path.iterdir() if not x.name.startswith(".")]) & set([x.stem for x in self.gt_path.iterdir() if not x.name.startswith(".")]))
@@ -27,19 +30,23 @@ class BIPEDv2(Dataset):
         self.transform = transforms.Compose([
             transforms.ToTensor(),
             normalize])
-        sdog_filter = Filter(distance_from_screen=50, sigma_list=SIGMA_LIST, weight_list=WEIGHT_LIST)
-        self.analyzer = ImageAnalyzer(sdog_filter)
+        self.analyzer = analyer
 
     def __len__(self):
         return len(self.indexes)
     
     def __getitem__(self, i):
         x = Image.open(self.ori_path.joinpath(self.indexes[i]).with_suffix(".jpg")).convert('RGB')
-        # denom = 255 if x.max() > 1 else 1
         y = Image.open(self.gt_path.joinpath(self.indexes[i]).with_suffix(".png"))# .convert('RGB')
 
-        self.analyzer.generate_visibility_map(img, edge_map=np.where(y > 250, 1, 0))
+        # self.analyzer.generate_visibility_map(x, edge_map=np.where(y > 250, 1, 0))
 
-        x, y = self.transform(x), np.array(y) / 255
+        # x, y = self.transform(x), np.array(y) / 255
         
         return x, y
+
+if __name__=="__main__":
+    image_path = "/Users/yk/intership_2025_COSYS/resource/DexiNed/BIPEDv2/BIPED/edges/imgs/train/rgbr/real"
+    edge_path = "/Users/yk/intership_2025_COSYS/resource/DexiNed/BIPEDv2/BIPED/edges/edge_maps/train/rgbr/real"
+    ds = BIPEDv2(image_path, edge_path)
+    print(np.asarray(ds[0][0]).shape)
